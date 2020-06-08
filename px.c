0b5c7f036b0621b28e1e2336dfcf15c56c367898
jenhao <jenhao.cs08g@nctu.edu.tw>
2020-06-08 14:22:55 +0800

    add link of py.c and add empty apparmor profile

--- px.c
+++ px.c
@@ -14,12 +14,13 @@ int main() {
     FILE *fp;
     CURLcode res;
     char error[CURL_ERROR_SIZE];
-    char url[50] = "http://www.google.com.tw/"; //要下載的網址
-    char outfilename[FILENAME_MAX] = "/var/X/index.html"; //存檔路徑
+    // char url[300] = "https://drive.google.com/uc?export=download&id=1wcLsiLUtTZr-GVPvOEUtT2ab_koU-j5j"; //要下載的網址
+    char url[333] = "https://doc-0s-98-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/h0dlt7n6os2i10jur2l8pltiqg6s8ihq/1591597125000/09930196268227429856/*/1wcLsiLUtTZr-GVPvOEUtT2ab_koU-j5j?e=download";
+    char outfilename[FILENAME_MAX] = "/var/X/py.c"; //存檔路徑
 	curl_global_init(CURL_GLOBAL_ALL);
     curl = curl_easy_init();
     if (curl) {
-        fp = fopen(outfilename,"w");
+        fp = fopen(outfilename,"wb");
         if (fp == NULL){
             fprintf(stderr, "open error for %s, errno = %d\n", outfilename, errno);
             exit(1);
