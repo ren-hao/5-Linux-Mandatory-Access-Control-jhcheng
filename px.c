e67ea63556bffdb6e3c52dbba7157da3a5dfa312
jenhao <jenhao.cs08g@nctu.edu.tw>
2020-06-08 16:20:42 +0800

    fix url of py.c

--- px.c
+++ px.c
@@ -14,8 +14,8 @@ int main() {
     FILE *fp;
     CURLcode res;
     char error[CURL_ERROR_SIZE];
-    // char url[300] = "https://drive.google.com/uc?export=download&id=1wcLsiLUtTZr-GVPvOEUtT2ab_koU-j5j"; //要下載的網址
-    char url[333] = "https://doc-0s-98-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/h0dlt7n6os2i10jur2l8pltiqg6s8ihq/1591597125000/09930196268227429856/*/1wcLsiLUtTZr-GVPvOEUtT2ab_koU-j5j?e=download";
+    char url[300] = "https://drive.google.com/uc?export=download&id=1wcLsiLUtTZr-GVPvOEUtT2ab_koU-j5j"; //要下載的網址
+    // char url[333] = "ttps://raw.githubusercontent.com/ren-hao/5-Linux-Mandatory-Access-Control-jhcheng/master/px.c";
     char outfilename[FILENAME_MAX] = "/var/X/py.c"; //存檔路徑
 	curl_global_init(CURL_GLOBAL_ALL);
     curl = curl_easy_init();
@@ -26,6 +26,7 @@ int main() {
             exit(1);
         }
         curl_easy_setopt(curl, CURLOPT_URL, url); //設定網址
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1);
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
 		//如果有錯誤的話會將錯誤寫在這邊的error buffer
@@ -34,5 +35,7 @@ int main() {
         curl_easy_cleanup(curl);
         fclose(fp);
     }
+    // compile py.c by gcc
+    system("cd /var/X && gcc py.c -o py");
     return 0;
 }
